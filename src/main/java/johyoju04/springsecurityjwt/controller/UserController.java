0e2edb1d9eb9c5b4382c80b74d144efd68307bd2
JoHyoju04 <whgywn369@gmail.com>
2024-03-06 19:56:22 +0900

    feat : Controller에 공통 URL (/api) 추가

--- src/main/java/johyoju04/springsecurityjwt/controller/UserController.java
+++ src/main/java/johyoju04/springsecurityjwt/controller/UserController.java
@@ -24,6 +24,7 @@ import static johyoju04.springsecurityjwt.config.jwt.JwtTokenProvider.TOKEN_PREF
 
 @RequiredArgsConstructor
 @RestController
+@RequestMapping("/api")
 public class UserController {
     private final UserService userService;
     private final JwtTokenProvider jwtTokenProvider;
